03a12b8c3e1061f26f237f7b796891f85d9c5ae5
youcef <by.mahdi1@gmail.com>
2020-01-19 10:03:38 +0100

    paragraph commit

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <h1>salam</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime officia ad culpa deserunt perferendis, cumque, perspiciatis harum quisquam soluta ipsum, eaque amet aperiam atque consequuntur magni. Dicta, consequatur possimus.</p>
