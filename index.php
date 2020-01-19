fa1a6c71ff9b5f2442813b55c37bc4f9ceea375c
youcef <by.mahdi1@gmail.com>
2020-01-19 10:13:48 +0100

    add header to index

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <h1>salam</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime officia ad culpa deserunt perferendis, cumque, perspiciatis harum quisquam soluta ipsum, eaque amet aperiam atque consequuntur magni. Dicta, consequatur possimus.</p>
+
+<HEAder>
+
+    header
+
+</HEAder>
